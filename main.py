d627b17001c2583a523f24769dc7c9ad618483de
manojmanu9441 <dreamers2k22@gmail.com>
2025-03-21 11:57:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template,redirect,request
 import joblib
 import warnings
 warnings.filterwarnings('ignore')
-
+import os
 app=Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
